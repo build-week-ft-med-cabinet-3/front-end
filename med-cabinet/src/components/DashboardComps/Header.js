1cfd9f6ddff4432d1317311537a27828c419d395
Isaac Gorman <emailme@isaacg.net>
2020-09-24 14:22:15 -0500

    prog 4

--- med-cabinet/src/components/DashboardComps/Header.js
+++ med-cabinet/src/components/DashboardComps/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import medSwap from "./assets/medSwap.svg";
+import user from "./assets/user.svg";
 
 const HeaderContainer = styled.div`
   width: 100vw;
@@ -13,13 +15,35 @@ const HeaderContainer = styled.div`
 const ComponentWrapper = styled.div`
   width: 95%;
   height: 40px;
-  background: #f3f3f3;
+  // background: #f3f3f3;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  // align-self: flex-end;
+`;
+
+const Logo = styled.img`
+  width: 100px;
+  // flex-grow: 7;
+  // height: 20px;
+  // margin-left: 40px;
+`;
+
+const Profile = styled.img`
+  width: 40px;
+  // flex-grow: 1;
+  // align-self: center;
+  // height: 20px;
+  // margin-left: 40px;
 `;
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <ComponentWrapper></ComponentWrapper>
+      <ComponentWrapper>
+        <Logo src={medSwap} alt="" />
+        <Profile src={user} alt="" />
+      </ComponentWrapper>
     </HeaderContainer>
   );
 };
