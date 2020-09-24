f060a6c279be5b73ff67f820a1947d1571ac923d
CStank89 <C.Stank89@gmail.com>
2020-09-24 18:31:37 -0400

    took border off since i cant figure out how to style it

--- med-cabinet/src/components/FormContainer.js
+++ med-cabinet/src/components/FormContainer.js
@@ -11,7 +11,7 @@ const FormWrapper = withStyles({
     flexDirection: "column",
     width: "100%",
     height: "100vh",
-    border: "1px solid black",
+    // border: "1px solid black",
   },
 })(Box);
 
