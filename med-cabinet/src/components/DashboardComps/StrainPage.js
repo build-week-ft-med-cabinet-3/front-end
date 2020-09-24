50e68d88d9190fde07bb463a6c2c83fa13418e97
Isaac Gorman <emailme@isaacg.net>
2020-09-24 13:44:05 -0500

    prog 2

--- med-cabinet/src/components/DashboardComps/StrainPage.js
+++ med-cabinet/src/components/DashboardComps/StrainPage.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Header from "./Header";
+import SavedStrains from "./SavedStrains";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
+import saveIcon from "./assets/saveIcon.svg";
 
 const StrainPageContainer = styled.div`
   width: 100%;
@@ -31,13 +34,55 @@ const RightWrapper = styled.div`
   align-items: center;
 `;
 
+const LInfo = styled.div`
+  width: 300px;
+  height: 300px;
+  background: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Name = styled.h5`
+  font-size: 1em;
+`;
+
+const SaveButton = styled.div`
+  width: 90%;
+  height: 40px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  // background: yellow;
+`;
+
+const SaveTxt = styled.p`
+  font-size: 0.75em;
+`;
+
+const SaveIcon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
 const StrainPage = () => {
   const params = useParams();
   return (
-    <StrainPageContainer>
-      <LeftWrapper></LeftWrapper>
-      <RightWrapper></RightWrapper>
-    </StrainPageContainer>
+    <div>
+      <Header />
+      <SavedStrains />
+      <StrainPageContainer>
+        <LeftWrapper>
+          <LInfo>
+            <Name>Tahoe OG Kush</Name>
+            <SaveButton>
+              <SaveTxt>Save</SaveTxt>
+              <SaveIcon src={saveIcon} alt="save icon" />
+            </SaveButton>
+          </LInfo>
+        </LeftWrapper>
+        <RightWrapper></RightWrapper>
+      </StrainPageContainer>
+    </div>
   );
 };
 
